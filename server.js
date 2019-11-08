320f7c7f59f36c77c46605e0dfd4cd5e6dbfe547
Smith Gajjar <smith.gajjar@gmail.com>
2019-11-08 10:18:34 +0530

    wip: initial changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
+
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts') 
@@ -10,20 +11,20 @@ const authorRouter = require('./routes/authors')
 
 const bodyParser = require('body-parser')
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({limit: '10mb' , extended:false}))
+    app.set('view engine', 'ejs')
+    app.set('views', __dirname + '/views')
+    app.set('layout', 'layouts/layout')
+    app.use(expressLayouts)
+    app.use(express.static('public'))
+    app.use(bodyParser.urlencoded({limit: '10mb' , extended:false}))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true , useUnifiedTopology: true })
+    mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true , useUnifiedTopology: true })
 const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongodb')) 
+    db.on('error', error => console.error(error))
+    db.once('open', () => console.log('Connected to Mongodb')) 
 
-app.use('/', indexRouter)
-app.use('/authors', authorRouter)
+    app.use('/', indexRouter)
+    app.use('/authors', authorRouter)
 
-app.listen(process.env.PORT || 3000)
+    app.listen(process.env.PORT || 3000)
